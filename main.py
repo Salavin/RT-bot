5f2a0c63e8a645eba05dda8cb10c6d24ad03ce52
Salavin <zeldatwili1@gmail.com>
2021-06-14 22:27:38 -0500

    Minor tweaks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.utils import get
-import constants
+import config
 
 client = discord.Client()
 
@@ -12,33 +12,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if (message.author == client.user) or (message.channel.id != constants.CHANNEL_ID):
+    if (message.author == client.user) or (message.channel.id != config.CHANNEL_ID):
         return
 
     roles = message.channel.guild.roles
     words = message.content.split(' ')
-    if words[0].lower() not in constants.COMMANDS:
+    if words[0].lower() not in config.COMMANDS:
         await message.author.send("Sorry, but `" + words[0] + "` is not a valid command!")
         await message.delete()
         return
     else:
-        command = constants.ADD if words[0].lower() == constants.ADD else constants.REMOVE
+        command = config.ADD if words[0].lower() == config.ADD else config.REMOVE
 
     successfulRoles = []
     unsuccessfulRoles = []
 
     for word in words[1:]:
-        if '-' in word and word.split('-')[0].upper() in constants.CLASS_SPECIFIERS:
+        if '-' in word and word.split('-')[0].upper() in config.CLASS_SPECIFIERS:
             for role in roles:
                 if role.name == word.upper():
-                    if command == constants.ADD:
+                    if command == config.ADD:
                         await message.author.add_roles(role)
                     else:
                         await message.author.remove_roles(role)
                     successfulRoles.append(word)
-        elif word in constants.MAJORS:
-            role = get(roles, id=constants.MAJORS.get(word))
-            if command == constants.ADD:
+        elif word in config.MAJORS:
+            role = get(roles, id=config.MAJORS.get(word))
+            if command == config.ADD:
                 await message.author.add_roles(role)
             else:
                 await message.author.remove_roles(role)
@@ -48,12 +48,12 @@ async def on_message(message):
 
     messageString = ""
     if successfulRoles:
-        messageString += "Successfully {verb} the following role(s):".format(verb="added" if command == constants.ADD else "removed")
+        messageString += "Successfully {verb} the following role(s):".format(verb="added" if command == config.ADD else "removed")
         for role in successfulRoles:
             messageString += " `{role}`,".format(role=role)
         messageString = messageString[:-1] + '\n'
     if unsuccessfulRoles:
-        messageString += "Had problems {verb} the following role(s):".format(verb="adding" if command == constants.ADD else "removing")
+        messageString += "Had problems {verb} the following role(s):".format(verb="adding" if command == config.ADD else "removing")
         for role in unsuccessfulRoles:
             messageString += " `{role}`,".format(role=role)
         messageString = messageString[:-1] + "\nCheck for any typos you may have made, and check to see if the role is on the list. If it's not, ping an admin, and they can add it for you."
@@ -61,4 +61,4 @@ async def on_message(message):
     await message.author.send(messageString)
     await message.delete()
 
-client.run(constants.TOKEN)
+client.run(config.TOKEN)
