410436aabf5ff8bdca4046b6bc519d359576f6b9
Salavin <zeldatwili1@gmail.com>
2021-06-16 15:05:46 -0500

    Major changes/additions.

---
+++ README.md
@@ -0,0 +1,16 @@
+<img src="RT_Logo.png" alt="Logo" title = "Logo" align="right" width="100" height="100" />
+
+# RT Bot
+## About
+A bot made for the Runtime Terror Discord server. The purpose of the bot is to give users the ability to easily add/remove roles from themselves for classes and majors.
+## Configuration
+You will need to add your own `config.py` if you would like to fork this repo and run it yourself. The file must include:
+* `TOKEN`: This is the token that you get when you create your bot. To see how to create your own bot and how to find your token, check out [this tutorial](https://discordpy.readthedocs.io/en/latest/discord.html).
+* `CHANNEL_ID`: The channel that the bot will listen for implicit commands in (without the `.` command prefix).
+* `CLASS_SPECIFIERS`: The class acronyms before the listing number.
+* `MAJORS`: A dictionary of key-value pairs, where the key is the exact name of the major role, and the value is the ID of the role.
+* `ADD`: A string identifying the `add` command.
+* `REMOVE`: A string identifying the `remove` command.
+* `COMMANDS`: A list containing `ADD` and `REMOVE`.
+## Contributing
+If you are from the Runtime Terror server, and you would like to contribute to this bot, please create an issue under the "Issues" tab.
--- main.py
+++ main.py
@@ -1,44 +1,56 @@
+import linecache
+import os
+import subprocess
+import sys
+import psutil
+
 import discord
 from discord.utils import get
+from discord.ext import commands
 import config
 
-client = discord.Client()
+client = commands.Bot(command_prefix='.', help_command=None)
 
 
-@client.event
-async def on_ready():
-    print("Logged in as {0.user}".format(client))
+def get_exception():
+    """Handling thrown exception from bot."""
+    _, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
 
 
-@client.event
-async def on_message(message):
-    if (message.author == client.user) or (message.channel.id != config.CHANNEL_ID):
-        return
+async def handle_roles(args, action, message):
+    """
+    Handles adding/removing roles to/from a user.
 
+    :param args: The list of roles to add/remove.
+    :param action: Whether to add or remove the list of roles.
+    :param message: The original message that was sent
+    """
     roles = message.channel.guild.roles
-    words = message.content.split(' ')
-    if words[0].lower() not in config.COMMANDS:
-        await message.author.send("Sorry, but `" + words[0] + "` is not a valid command!")
-        await message.delete()
-        return
-    else:
-        command = config.ADD if words[0].lower() == config.ADD else config.REMOVE
-
     successfulRoles = []
     unsuccessfulRoles = []
 
-    for word in words[1:]:
+    for word in args:
         if '-' in word and word.split('-')[0].upper() in config.CLASS_SPECIFIERS:
+            if "491" in word or "492" in word:  # Special cases for Senior Design classes
+                newWord = "SE/COMS/CPRE/EE-" + "491" if "491" in word else "492"
+            else:
+                newWord = word.upper()
             for role in roles:
-                if role.name == word.upper():
-                    if command == config.ADD:
+                if role.name == newWord:
+                    if action == config.ADD:
                         await message.author.add_roles(role)
                     else:
                         await message.author.remove_roles(role)
                     successfulRoles.append(word)
         elif word in config.MAJORS:
             role = get(roles, id=config.MAJORS.get(word))
-            if command == config.ADD:
+            if action == config.ADD:
                 await message.author.add_roles(role)
             else:
                 await message.author.remove_roles(role)
@@ -48,17 +60,99 @@ async def on_message(message):
 
     messageString = ""
     if successfulRoles:
-        messageString += "Successfully {verb} the following role(s):".format(verb="added" if command == config.ADD else "removed")
+        messageString += "Successfully {verb} the following role(s):".format(
+            verb="added" if action == config.ADD else "removed")
         for role in successfulRoles:
             messageString += " `{role}`,".format(role=role)
         messageString = messageString[:-1] + '\n'
     if unsuccessfulRoles:
-        messageString += "Had problems {verb} the following role(s):".format(verb="adding" if command == config.ADD else "removing")
+        messageString += "Had problems {verb} the following role(s):".format(
+            verb="adding" if action == config.ADD else "removing")
         for role in unsuccessfulRoles:
             messageString += " `{role}`,".format(role=role)
-        messageString = messageString[:-1] + "\nCheck for any typos you may have made, and check to see if the role is on the list. If it's not, ping an admin, and they can add it for you."
+        messageString = messageString[
+                        :-1] + "\nCheck for any typos you may have made, and check to see if the role is on the list. If it's not, ping an admin, and they can add it for you."
     messageString.rstrip('\n')
     await message.author.send(messageString)
     await message.delete()
+
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    try:
+        if (message.author == client.user) or (message.channel.id != config.CHANNEL_ID):
+            return
+
+        words = message.content.split(' ')
+        if words[0].lower() not in config.COMMANDS:
+            for command in client.commands:
+                if words[0][1:] == command.name or words[0][1:] in command.aliases:
+                    await client.process_commands(message)
+                    return
+            await message.author.send("Sorry, but `" + words[0] + "` is not a valid command!")
+            await message.delete()
+            await client.process_commands(message)
+            return
+        else:
+            command = config.ADD if words[0].lower() == config.ADD else config.REMOVE
+
+        await handle_roles(words[1:], command, message)
+    except Exception:
+        await message.channel.send("Oh no, I threw an error! <@262043915081875456>")
+        await message.channel.send("```" + get_exception() + "```")
+        print(get_exception())
+
+
+class Commands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @client.command(brief="Adds roles to a user based on the list of roles given.")
+    async def add(self, *args):
+        """
+        Usage: `!add <name(s) of role(s)>`
+
+        Adds roles to a user based on the list of roles given.
+        """
+        await handle_roles(list(set([i for i in args])), config.ADD, self.message)
+
+    @client.command(aliases=["rm"], brief="Removes roles from a user based on the list of roles given.")
+    async def remove(self, *args):
+        """
+        Usage: `!(remove | rm) <name(s) of role(s)>`
+
+        Removes roles from a user based on the list of roles given.
+        """
+        await handle_roles(list(set([i for i in args])), config.REMOVE, self.message)
+
+    @client.command()
+    @commands.has_role(712353676299075704)
+    async def restart(self):
+        """Restarts the bot, given the user has the correct role."""
+        await self.send("Restarting...")
+        print("Shutting down")
+        sys.exit()
+
+    @client.command()
+    @commands.has_role(712353676299075704)
+    async def stats(self):
+        """Shows the uptime and memory usage for the bot."""
+        p = subprocess.Popen("uptime", stdout=subprocess.PIPE, shell=True)
+        (output, _) = p.communicate()
+        await self.send(f"Uptime: `{str(output)[3: -3]}`")
+        process = psutil.Process(os.getpid())
+        await self.send(f"Memory: `{str(process.memory_info().rss / float(1000000))} mb`")
+
+    @client.command()
+    @commands.has_role(712353676299075704)
+    async def ping(self):
+        """Shows the current ping for the bot."""
+        await self.send(f"Pong! (`{str(round(client.latency, 3))} s`)")
+
 
 client.run(config.TOKEN)
